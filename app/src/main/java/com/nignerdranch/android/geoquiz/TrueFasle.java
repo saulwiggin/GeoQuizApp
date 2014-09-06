7efa74c545786830f7b1f237978ccd394e5afa85
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-06 15:09:20 +0100

    android

---
+++ app/src/main/java/com/nignerdranch/android/geoquiz/TrueFasle.java
@@ -0,0 +1,32 @@
+package com.nignerdranch.android.geoquiz;
+
+/**
+ * Created by Icarus on 9/6/2014.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrue = trueQuestion;
+    }
+
+    public int getQuestion() {
+        mQuestion = question;
+    }
+
+    public void setQuestion() {
+        return mTrueQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}

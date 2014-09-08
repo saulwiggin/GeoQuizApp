b1f5f839df9a74d76a7116d38b6e5b3fc199c63d
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-08 21:30:24 +0100

    create cheat_activity.java

---
+++ app/src/main/java/com/nignerdranch/android/geoquiz/CheatActivity/java.java
@@ -0,0 +1,16 @@
+package com.nignerdranch.android.geoquiz.CheatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Icarus on 9/8/2014.
+ */
+public class java {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+           super.onCreate(savedInstanceState);
+            setContentView(R.layout.actuivity_cheat);
+        
+    }
+}

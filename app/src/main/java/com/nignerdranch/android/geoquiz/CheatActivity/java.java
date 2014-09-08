7a88a3ab5d7ca3f55951c96622a48b254b0c3f69
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-08 21:50:49 +0100

    using an extra

--- app/src/main/java/com/nignerdranch/android/geoquiz/CheatActivity/java.java
+++ app/src/main/java/com/nignerdranch/android/geoquiz/CheatActivity/java.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
  */
 public class java {
 
+    private boolean mAnswerIsTrue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
            super.onCreate(savedInstanceState);
             setContentView(R.layout.actuivity_cheat);
 
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
     }
 }

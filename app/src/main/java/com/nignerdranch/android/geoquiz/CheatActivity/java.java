138e2436eafcc9ee3b2c2b574a0f77cba81b5024
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-08 21:32:31 +0100

    add to androidmanifest.xml

--- app/src/main/java/com/nignerdranch/android/geoquiz/CheatActivity/java.java
+++ app/src/main/java/com/nignerdranch/android/geoquiz/CheatActivity/java.java
@@ -11,6 +11,6 @@ public class java {
     protected void onCreate(Bundle savedInstanceState){
            super.onCreate(savedInstanceState);
             setContentView(R.layout.actuivity_cheat);
-        
+
     }
 }

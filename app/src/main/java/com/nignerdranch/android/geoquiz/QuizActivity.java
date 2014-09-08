cae16f39775a1732a53381bb6db874df1112fc71
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-08 21:47:17 +0100

    putting extra in quizactivity

--- app/src/main/java/com/nignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/nignerdranch/android/geoquiz/QuizActivity.java
@@ -59,6 +59,8 @@ public class QuizActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
                 startActivity(i);
                 }
         }
